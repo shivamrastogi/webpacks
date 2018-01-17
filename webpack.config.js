7728871db46e9859913855d23ea66ea765859a37
Shivam Rastogi <shivam1.rastogi@paytm.com>
2018-01-17 12:20:24 +0530

    source-maps-and-dist-creation

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: []
+  },
+  plugins: [new UglifyJSPlugin({sourceMap: true})]
+};
